eec99dbd27c5d74a7fbc907ac4f4336c29f3face
yunjaeeun <yunjaeeun12@naver.com>
2025-02-13 17:20:05 +0900

    ignore 추가

--- llm_stt_test_files/test.py
+++ llm_stt_test_files/test.py
@@ -4,3 +4,4 @@ dataset_name = "mozilla-foundation/common_voice_12_0"
 available_languages = get_dataset_config_names(dataset_name)
 
 print("지원되는 언어 목록:", available_languages)
+print("지원되는 언어 목록:", available_languages)
